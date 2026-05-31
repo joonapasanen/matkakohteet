14e2b06e6798389b4b4f078b861a83f4c2b3b05d
Joona <joona.t.pasanen@gmail.com>
2026-05-31 22:50:06 +0300

    added creating user

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
+import sqlite3
 from flask import Flask
 from flask import render_template, request
+from werkzeug.security import generate_password_hash
+
+import db
 
 app = Flask(__name__)
 
@@ -7,9 +11,26 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
