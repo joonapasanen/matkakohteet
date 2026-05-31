1b268b9ac7fdd8488a1d7a0cb4ead65442b77ae7
Joona <joona.t.pasanen@gmail.com>
2026-05-31 20:58:07 +0300

    added index.html

--- src/app.py
+++ src/app.py
@@ -1,16 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "root"
+    return render_template("index.html")
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET, POST'])
 def register():
     pass
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET, POST'])
 def login():
     pass
 
